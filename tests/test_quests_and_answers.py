a46eb3060c3a4cadb93dcd5160da6f8fcff22007
Alexander Dneprov <dneprov.1996@yandex.ru>
2024-04-26 19:31:02 +0200

    public_tests
    убраны прямые обращения к driver из тестов, только через page
    драйвер больше не передается в методы, является атрибутом page
    
    все прямые обращения к driver вынесены в base и оттуда за счет наследования переиспользованы в объектах страниц
    
    в order_pages_1.py и order_pages_2.py изменен неймиинг классов
    
    применена параметризация в файле test_quests_and_answers

--- tests/test_quests_and_answers.py
+++ tests/test_quests_and_answers.py
@@ -7,7 +7,7 @@ from pages.main_page import MainPageQuests, MainPageAnswers
 class TestQuestsAndAnswers:
 
     @allure.title("Проверка Вопроса {quest_number}")
-    @allure.description("Проверяем что Вопрос {quest_number} раскрывается при нажатии на него")
+    @allure.description("Проверяем что каждый вопрос раскрывается при нажатии на него")
     @pytest.mark.parametrize("quest_number, quest_element, answer_element, answer_text", [
         (1, MainPageQuests.QUEST_1, MainPageAnswers.ANSWER_1, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."),
         (2, MainPageQuests.QUEST_2, MainPageAnswers.ANSWER_2, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."),
@@ -18,19 +18,13 @@ class TestQuestsAndAnswers:
         (7, MainPageQuests.QUEST_7, MainPageAnswers.ANSWER_7, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."),
         (8, MainPageQuests.QUEST_8, MainPageAnswers.ANSWER_8, "Да, обязательно. Всем самокатов! И Москве, и Московской области."),
     ])
+
     def test_click_quest(self, driver, quest_number, quest_element, answer_element, answer_text):
         main_page_quest = MainPageQuests(driver)
-        main_page_quest.open_page(URL.YASCOOTER)
-
-        # Находим элемент
-        quest = main_page_quest.wait_and_find_element(quest_element)
-
-        # Скроллируем до вопроса и кликаем на него
-        main_page_quest.scroll_into_view(quest)
-        quest.click()
-
         main_page_answer = MainPageAnswers(driver)
 
-        # Проверяем наличие ответа и его текст
-        answer = main_page_answer.wait_and_find_element(answer_element)
-        assert answer.text == answer_text
+        main_page_quest.open_page(URL.YASCOOTER)
+        main_page_quest.scroll_to_quest(quest_element)
+        main_page_quest.find_and_click_quest(quest_element)
+
+        assert main_page_answer.find_answer(answer_element)
